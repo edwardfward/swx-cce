5d17244b25eab2f20bd17fabfa6313fc6b8fa3dc
Edward F. Ward III <4785890+edwardfward@users.noreply.github.com>
2018-12-04 15:46:25 -0500

    applied go fmt

--- cmd/cce-server.go
+++ cmd/cce-server.go
@@ -5,15 +5,15 @@ import (
 	"os"
 )
 
-func main(){
+func main() {
 
 	dbUrl := os.Getenv("DB_URL")
 	dbUsername := os.Getenv("DB_USERNAME")
 	dbPassword := os.Getenv("DB_PASSWORD")
 	dbPort := os.Getenv("DB_PORT")
 
-	fmt.Printf("Database Url: %s\n" +
-		"Database Username: %s\n" +
-		"Database Password: %s\n" +
+	fmt.Printf("Database Url: %s\n"+
+		"Database Username: %s\n"+
+		"Database Password: %s\n"+
 		"Database Port: %s\n", dbUrl, dbUsername, dbPassword, dbPort)
 }
