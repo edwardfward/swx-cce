eb7ffcc5c85a32d7dc57c591a9b3eac8ecd40da7
Edward F. Ward III <4785890+edwardfward@users.noreply.github.com>
2018-12-05 10:09:22 -0500

    added /info handler for testing

--- cmd/cceapp.go
+++ cmd/cceapp.go
@@ -2,18 +2,36 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 	"os"
 )
 
 func main() {
 
+	http.HandleFunc("/info", displayEnvVars)
+
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		return
+	}
+}
+
+func displayEnvVars(w http.ResponseWriter, r *http.Request) {
 	dbUrl := os.Getenv("DB_URL")
+	dbPort := os.Getenv("DB_PORT")
 	dbUsername := os.Getenv("DB_USERNAME")
 	dbPassword := os.Getenv("DB_PASSWORD")
-	dbPort := os.Getenv("DB_PORT")
 
-	fmt.Printf("Database Url: %s\n"+
+	n, err := fmt.Fprintf(w, "Database Url: %s\n"+
+		"Database Port: %s\n"+
 		"Database Username: %s\n"+
-		"Database Password: %s\n"+
-		"Database Port: %s\n", dbUrl, dbUsername, dbPassword, dbPort)
+		"Database Password: %s\n", dbUrl, dbPort, dbUsername, dbPassword)
+
+	if n == 0 || err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		_, err := w.Write([]byte("500 - Database parameters did not write properly"))
+		if err != nil {
+			return
+		}
+	}
 }
