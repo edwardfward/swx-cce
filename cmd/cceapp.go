2273dd864377e6720c509642c8a592e389d055cb
Edward F. Ward III <4785890+edwardfward@users.noreply.github.com>
2018-12-05 11:17:16 -0500

    added db connection test and changed db_host env vars

--- cmd/cceapp.go
+++ cmd/cceapp.go
@@ -1,31 +1,48 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
+	"log"
 	"net/http"
 	"os"
 )
 
+var (
+	dbHost     = os.Getenv("DB_HOST")
+	dbName     = os.Getenv("DB_NAME")
+	dbPort     = os.Getenv("DB_PORT")
+	dbUsername = os.Getenv("DB_USERNAME")
+	dbPassword = os.Getenv("DB_PASSWORD")
+)
+
 func main() {
+
+	connStr := fmt.Sprintf("postgres://%s:%s@%s:%s/%s",
+		dbUsername, dbPassword, dbHost, dbPort, dbName)
+
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		log.Fatalf("DATABASE CONNECTION ERROR: Could not connect to database" +
+			" %s on %s:%s", dbName, dbHost, dbPort)
+	}
+
+	err = db.Ping()
+	if err != nil {
+		log.Fatal("ERROR: Failed database ping")
+	}
 
 	http.HandleFunc("/info", displayEnvVars)
 
-	err := http.ListenAndServe(":8080", nil)
-	if err != nil {
-		return
-	}
+	http.ListenAndServe(":8080", nil)
 }
 
 func displayEnvVars(w http.ResponseWriter, r *http.Request) {
-	dbUrl := os.Getenv("DB_URL")
-	dbPort := os.Getenv("DB_PORT")
-	dbUsername := os.Getenv("DB_USERNAME")
-	dbPassword := os.Getenv("DB_PASSWORD")
 
 	n, err := fmt.Fprintf(w, "Database Url: %s\n"+
 		"Database Port: %s\n"+
 		"Database Username: %s\n"+
-		"Database Password: %s\n", dbUrl, dbPort, dbUsername, dbPassword)
+		"Database Password: %s\n", dbHost, dbPort, dbUsername, dbPassword)
 
 	if n == 0 || err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
